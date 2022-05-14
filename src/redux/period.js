eb6fce5f3006d6bf732ae24b162f0893dc6ed610
kyus <kyus@aty.kr>
2022-05-14 15:12:02 +0900

    add redux state ..
    - 날짜 선택에 필요한 시 / 분 데이터 저장 state 추가
    -- stHour / stMiunute / edHour / edMinute

--- src/redux/period.js
+++ src/redux/period.js
@@ -1,7 +1,11 @@
-export const SET_START_DATE = "PERIOD/SET_START_DATE";
-export const SET_END_DATE = "PERIOD/SET_END_DATE";
-export const SET_START_TMP_DATE = "PERIOD/SET_START_TMP_DATE";
-export const SET_END_TMP_DATE = "PERIOD/SET_END_TMP_DATE";
+const SET_START_DATE = "PERIOD/SET_START_DATE";
+const SET_END_DATE = "PERIOD/SET_END_DATE";
+const SET_START_TMP_DATE = "PERIOD/SET_START_TMP_DATE";
+const SET_END_TMP_DATE = "PERIOD/SET_END_TMP_DATE";
+const SET_START_HOUR = "PERIOD/SET_START_HOUR";
+const SET_START_MINUTE = "PERIOD/SET_START_MINUTE";
+const SET_END_HOUR = "PERIOD/SET_END_HOUR";
+const SET_END_MINUTE = "PERIOD/SET_END_MINUTE";
 
 const _getType = (type) => {
     switch (type) {
@@ -13,6 +17,14 @@ const _getType = (type) => {
             return SET_END_DATE;
         case "endTmpDate":
             return SET_END_TMP_DATE;
+        case "stH":
+            return SET_START_HOUR;
+        case "stM":
+            return SET_START_MINUTE;
+        case "edH":
+            return SET_END_HOUR;
+        case "edM":
+            return SET_END_MINUTE;
         default:
             return "";
     }
@@ -24,6 +36,10 @@ const initialState = {
     endDate: false,
     startTmpDate: false,
     endTmpDate: false,
+    stHour: 0,
+    stMinute: 0,
+    edHour: 0,
+    edMinute: 0,
 };
 
 const period = (state = initialState, action) => {
@@ -36,6 +52,14 @@ const period = (state = initialState, action) => {
             return { ...state, endDate: action.date };
         case SET_END_TMP_DATE:
             return { ...state, endTmpDate: action.date };
+        case SET_START_HOUR:
+            return { ...state, stHour: action.date };
+        case SET_START_MINUTE:
+            return { ...state, stMinute: action.date };
+        case SET_END_HOUR:
+            return { ...state, edHour: action.date };
+        case SET_END_MINUTE:
+            return { ...state, edMinute: action.date };
         default:
             return state;
     }
